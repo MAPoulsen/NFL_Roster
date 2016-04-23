8da3bc6032dda679a16d4ecdfb04cea3aaa05bf3
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-04-23 13:01:01 -0600

    aw

--- performer-services.js
+++ performer-services.js
@@ -1,92 +1,92 @@
-function PerformerService (url, callWhenDone){
+function PlayerService (url, callWhenDone){
   
   if(typeof callWhenDone !== 'function'){
       return 'Error: you must provide a function to call when done'
   }
     
   var self = this;
-  var performerData = []
+  var playerData = []
   
-  self.getAllPerformers = function(){
-      return performerData;
+  self.getAllPlayers = function(){
+      return playerData;
   }
   
-  self.getPerformerBySomeValue = function(value){
-    var troupe = [];
-    for (var i = 0; i < performerData.length; i++) {
-        var currentPerformer = performerData[i];
+  self.getPlayerBySomeValue = function(value){
+    var team = [];
+    for (var i = 0; i < playerData.length; i++) {
+        var currentPlayer = playerData[i];
         var hasValue = false;
-        for(var prop in currentPerformer){
-            if(typeof currentPerformer[prop] === 'string' && currentPerformer[prop].toLowerCase() === value){
+        for(var prop in currentPlayer){
+            if(typeof currentPlayer[prop] === 'string' && currentPlayer[prop].toLowerCase() === value){
                 hasValue = true;
             }   
         }
         if(hasValue){
-            troupe.push(currentPerformer)
+            team.push(currentPlayer)
         }
     }
-    return troupe;      
+    return team;      
   } 
   
-}
+
    
   
   
   
   
-//   function goGetData(){
-//       var BCWServer = "http://bcw-getter.herokuapp.com/?url=";
-//       var modifiedUrl = BCWServer + encodeURIComponent(url);
+  function goGetData(){
+      var BCWServer = "http://bcw-getter.herokuapp.com/?url=";
+      var modifiedUrl = BCWServer + encodeURIComponent(url);
       
-//       $.get(modifiedUrl, function(response){
-//           var data = JSON.parse(response)
-//           performerData = data.body.performer.filter(function(performer){
-//               if(performer.pro_status === 'A'){
-//                   return player;
-//               }
-//           })
-//           callWhenDone(self)
-//       })
+      $.get(modifiedUrl, function(response){
+          var data = JSON.parse(response)
+          playerData = data.body.player.filter(function(player){
+              if(player.pro_status === 'A'){
+                  return player;
+              }
+          })
+          callWhenDone(self)
+      })
       
-//   }
+  }
   
-//   goGetData()
+  goGetData()
   
-// }
-
-
-
-
-
-
-
-
-
-function goGetData (){
-    console.log('performerData')
-    console.log(1)
-    console.log(2)
-    console.log(3)
-    
-    performerData = [{
-      name: 'Diamond Kitty',
-      act: 'Fat-Lady',
-     ring: 'small-tents',
-        
-    },{
-     name: 'Colin the Amazing',
-      act: 'Knife Thrower',
-     ring: 'medium-tents'
-    },{
-        name: 'Stephan',
-      act: 'Lion Tamer',
-     ring: 'big-tents'
-    },{
-        name: 'Anna-Bell',
-      act: 'Magician',
-     ring: 'small-tents'
-    }]
-    
-    callWhenDone(self)
 }
-goGetData()
+
+
+
+
+
+
+
+
+
+// function goGetData (){
+//     console.log('performerData')
+//     console.log(1)
+//     console.log(2)
+//     console.log(3)
+    
+//     performerData = [{
+//       name: 'Diamond Kitty',
+//       act: 'Fat-Lady',
+//      ring: 'small-tents',
+        
+//     },{
+//      name: 'Colin the Amazing',
+//       act: 'Knife Thrower',
+//      ring: 'medium-tents'
+//     },{
+//         name: 'Stephan',
+//       act: 'Lion Tamer',
+//      ring: 'big-tents'
+//     },{
+//         name: 'Anna-Bell',
+//       act: 'Magician',
+//      ring: 'small-tents'
+//     }]
+    
+//     callWhenDone(self)
+// }
+// goGetData()
